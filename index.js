594181b7ea2df184880592e3eeca62840a2b080a
MariuszWiacek <wiacekm.uber@gmail.com>
2023-02-17 11:02:50 +0000

    mobile version

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var clearEl = document.getElementById("clear-button");
-var storage = JSON.parse(localStorage.getItem('history')) || [];
 var MyButton = $('#history');
 var userInput = $('#search-input')
     
@@ -16,8 +15,6 @@ $("#search-button").on("click", function(event) {
   $('#today').css("visibility", "visible");
   var Image = ('<img id="theImg" src="./images/conures.jpg" />')
 $('#today').prepend(Image);
-if (Image.length > 1) {
-              $('#today').remove(Image)}
 
 // capitalize userInput name to make sure is in capital letters even if user inputs in lower case
 })
@@ -28,7 +25,7 @@ $("#clear-button").on("click", function(event) {
     event.preventDefault();
     $("#today").html("") 
     $('#today').css("visibility", "visible");
-  $('#today').prepend('<img id="theImg" src="./images/seagull.webp" />')
+  $('#today').prepend('<img id="theImg2" src="./images/seagull.webp" />')
   
   //   $("#forecast-header").css("visibility", "visible");
   //   $("#forecast").css("visibility", "visible");
