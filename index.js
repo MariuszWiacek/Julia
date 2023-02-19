c2c99f084d2aeeaa3f5efea008709a8a0a281ffb
MariuszWiacek <wiacekm.uber@gmail.com>
2023-02-19 10:21:58 +0000

    albatrosses

--- index.js
+++ index.js
@@ -11,28 +11,31 @@ var userInput = $('#search-input')
 
 
  
-$("#search-button").on("click", function(event) {
+$("#red-button").on("click", function(event) {
     event.preventDefault();
     $("#today").html("") 
   $('#today').css("visibility", "visible");
   var Image = ('<img id="theImg" src="./images/conures.jpg" />')
 $('#today').prepend(Image);
 
-// capitalize userInput name to make sure is in capital letters even if user inputs in lower case
+
 })
 
 
 
-$("#clear-button").on("click", function(event) {
+$("#blue-button").on("click", function(event) {
     event.preventDefault();
     $("#today").html("") 
     $('#today').css("visibility", "visible");
   $('#today').prepend('<img id="theImg2" src="./images/seagull.webp" />')
-  
-  //   $("#forecast-header").css("visibility", "visible");
-  //   $("#forecast").css("visibility", "visible");
-  
-  
-  // capitalize userInput name to make sure is in capital letters even if user inputs in lower case
   })
+
+  $("#orange-button").on("click", function(event) {
+    event.preventDefault();
+    $("#today").html("") 
+    $('#today').css("visibility", "visible");
+  $('#today').prepend('<img id="theImg2" src="./images/albatross.jpeg" />')
+  })
+
+  
   
